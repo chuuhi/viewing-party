56516bc62b3c269d5410d8af6839e6a2ecdfe710
rayne <pothong.r@gmail.com>
2026-09-14 14:56:23 -0700

    finished wave 1

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -56,8 +56,6 @@ def test_create_no_rating_movie():
     assert new_movie is None
 
 
-
-@pytest.mark.skip()
 def test_adds_movie_to_user_watched():
     # Arrange
     movie = {
@@ -78,7 +76,7 @@ def test_adds_movie_to_user_watched():
     assert updated_data["watched"][0]["genre"] == GENRE_1
     assert updated_data["watched"][0]["rating"] == RATING_1
 
-@pytest.mark.skip()
+
 def test_adds_movie_to_non_empty_user_watched():
     # Arrange
     movie = {
@@ -98,7 +96,6 @@ def test_adds_movie_to_non_empty_user_watched():
     assert movie in updated_data["watched"]
     assert FANTASY_2 in updated_data["watched"]
 
-@pytest.mark.skip()
 def test_adds_movie_to_user_watchlist():
     # Arrange
     movie = {
@@ -119,7 +116,6 @@ def test_adds_movie_to_user_watchlist():
     assert updated_data["watchlist"][0]["genre"] == GENRE_1
     assert updated_data["watchlist"][0]["rating"] == RATING_1
 
-@pytest.mark.skip()
 def test_adds_movie_to_non_empty_user_watchlist():
     # Arrange
     movie = {
@@ -139,7 +135,6 @@ def test_adds_movie_to_non_empty_user_watchlist():
     assert movie in updated_data["watchlist"]
     assert FANTASY_2 in updated_data["watchlist"]
 
-@pytest.mark.skip()
 def test_moves_movie_from_watchlist_to_empty_watched():
     # Arrange
     janes_data = {
@@ -153,17 +148,19 @@ def test_moves_movie_from_watchlist_to_empty_watched():
 
     # Act
     updated_data = watch_movie(janes_data, MOVIE_TITLE_1)
-
+    movie_to_watch = {
+            "title": MOVIE_TITLE_1,
+            "genre": GENRE_1,
+            "rating": RATING_1
+        }
     # Assert
     assert len(updated_data["watchlist"]) == 0
     assert len(updated_data["watched"]) == 1
-    
-    raise Exception("Test needs to be completed.")
-    # *******************************************************************************************
-    # ****** Add assertions here to test that the correct movie was added to "watched" **********
-    # *******************************************************************************************
 
-@pytest.mark.skip()
+    assert movie_to_watch in updated_data["watched"]
+    assert movie_to_watch not in updated_data["watchlist"]
+
+
 def test_moves_movie_from_watchlist_to_watched():
     # Arrange
     movie_to_watch = HORROR_1
@@ -181,13 +178,10 @@ def test_moves_movie_from_watchlist_to_watched():
     # Assert
     assert len(updated_data["watchlist"]) == 1
     assert len(updated_data["watched"]) == 2
-    
-    raise Exception("Test needs to be completed.")
-    # *******************************************************************************************
-    # ****** Add assertions here to test that the correct movie was added to "watched" **********
-    # *******************************************************************************************
+    assert movie_to_watch in updated_data["watched"]
+    assert movie_to_watch not in updated_data["watchlist"]
 
-@pytest.mark.skip()
+
 def test_does_nothing_if_movie_not_in_watchlist():
     # Arrange
     movie_to_watch = HORROR_1
--- viewing_party/party.py
+++ viewing_party/party.py
@@ -13,7 +13,42 @@ def create_movie(title, genre, rating):
     }
     return movie
 
+def add_to_watched(user_data, movie):
 
+    if user_data.get("watched") is None:
+        user_data["watched"]=[]
+
+    if movie not in user_data["watched"]:
+        user_data["watched"].append(movie)
+
+    return user_data
+
+
+def add_to_watchlist(user_data, movie):
+
+    if user_data.get("watchlist") is None:
+        user_data["watchlist"]=[]
+
+    if movie not in user_data["watchlist"]:
+        user_data["watchlist"].append(movie)
+
+    return user_data
+
+
+def watch_movie(user_data, title):
+    if user_data is None or title is None:
+        return None
+
+    movie = get_movie_in_list(user_data["watchlist"],title)
+    if movie:
+        user_data["watchlist"].remove(movie)
+
+        if not user_data["watched"]:
+            user_data["watched"] = []
+
+        user_data["watched"].append(movie)
+
+    return user_data
 
 
 # -----------------------------------------
@@ -34,3 +69,17 @@ def create_movie(title, genre, rating):
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
 
+
+
+
+
+# =================== helper functions =====================
+def get_movie_in_list(user_list,title):
+    if title is None or user_list is None:
+        return None
+
+    for movie in user_list:
+        if movie["title"].lower() == title.lower():
+            return movie
+    return None
+# ================================================================
